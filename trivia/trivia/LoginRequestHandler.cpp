fa8d5937f3d3d7282899f397c80b05942ae31ff9
ron peer <ronpeer100@gmail.com>
2021-05-03 19:23:22 +0300

    complete

--- trivia/trivia/LoginRequestHandler.cpp
+++ trivia/trivia/LoginRequestHandler.cpp
@@ -1,4 +1,6 @@
 #include "LoginRequestHandler.h"
+#include "JsonResponsePacketSerializer.h"
+#include "ResponseStructs.h"
 
 bool LoginRequestHandler::isRequestRelevant(RequestInfo RI)
 {
@@ -9,5 +11,25 @@ bool LoginRequestHandler::isRequestRelevant(RequestInfo RI)
 }
 RequestResult LoginRequestHandler::handleRequest(RequestInfo RI)
 {
-	
+	RequestResult RR;
+	if (!isRequestRelevant(RI))
+	{
+		struct ErrorResponse ER;
+		ER.message = "the request is not relevent";
+		RR.buffer = JsonResponsePacketSerializer::serializeResponse(ER);
+	}
+	else if (RI.id == LOGIN_CODE)
+	{
+		struct LoginResponse LR;
+		LR.status = LOGIN_CODE;
+		RR.buffer = JsonResponsePacketSerializer::serializeResponse(LR);
+	}
+	else
+	{
+		struct SignupResponse SR;
+		SR.status = LOGIN_CODE;
+		RR.buffer = JsonResponsePacketSerializer::serializeResponse(SR);
+	}
+	RR.newHandler = NULL;
+	return RR;
 }
